3a5c9a460d63c4081da93c8b624beddcd560bcbf
Rasmus Thygesen <rasmus.thygesen.privat@gmail.com>
2020-11-18 02:31:51 +0100

    Set up for build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.10"
+    id("org.openjfx.javafxplugin") version "0.0.8"
 }
+
 group = "me.kt"
 version = "1.0-SNAPSHOT"
+
+javafx {
+    modules("javafx.controls", "javafx.fxml")
+}
 
 dependencies {
     implementation("no.tornado:tornadofx:1.7.20")
@@ -19,6 +25,15 @@ tasks.withType<KotlinCompile> {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
 }
 
 repositories {
